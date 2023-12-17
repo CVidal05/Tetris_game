56cf953e4b9943c6d9d47fa4e98ae956d8487768
Damag0 <153758175+Damag0@users.noreply.github.com>
2023-12-17 18:19:24 +0100

    Add files via upload

--- 6.3.py
+++ 6.3.py
@@ -72,10 +72,17 @@ class Game:
         self.lineas_surface.fill((0,255,0)) # La pinto de verde para luego quitarlo
         self.lineas_surface.set_colorkey((0,255,0))
         self.lineas_surface.set_alpha(120)
-            
-        self.pieza = Pieza(random.randint(0,6),self.grupo)
         
-    
+        # Pieza
+        # Lista de piezas para colisiones
+        self.matrizPiezas = []
+        for fil in range(FILAS):
+            self.matrizPiezas.append([])
+            for col in range(COLUMNAS):
+                self.matrizPiezas[fil].append(0)
+        # Pieza que está en juego --- llama a la clase Pieza ( tipo, grupo de cuadrados, función para crear otra pieza, Matriz para colisiones entre piezas)
+        self.pieza = Pieza(random.randint(6,6), self.grupo, self.crearNuevaPieza, self.matrizPiezas)
+       
     def dibujo_lineas(self):
         for col in range(1,COLUMNAS):
             pygame.draw.line(self.lineas_surface, Blanco, (col*tamañoCeldas,0),(col*tamañoCeldas,AltJuego), 1) # (ventana, color, pos inicial, pos final, anchura)
@@ -83,18 +90,63 @@ class Game:
             pygame.draw.line(self.lineas_surface, Blanco, (0,fil*tamañoCeldas),(LongJuego,fil*tamañoCeldas), 1)
             
         self.surface.blit(self.lineas_surface, (0,0)) # Blit en la surface de la ventana la surfaces de las lineas
-    
+        
     def moverbajo(self):
         self.pieza.moverbajo()
         
     def input(self):
+        
+        # Movimientos laterales
         teclas = pygame.key.get_pressed()
         if teclas[pygame.K_LEFT]:
             self.pieza.moverizq()
         if teclas[pygame.K_RIGHT]:
             self.pieza.moverder()
+        
+        # Rotación
+        if teclas[pygame.K_UP]:
+            self.pieza.rotacion()
             
     
+    def crearNuevaPieza(self):
+        self.lineaCompleta()
+        self.pieza = Pieza(random.randint(6,6), self.grupo, self.crearNuevaPieza, self.matrizPiezas)
+        
+    def lineaCompleta(self): # Comprueba si alguna fila está completa
+        cont = 0
+        borrarFil = []
+        for fil in range(FILAS):
+            for col in range(COLUMNAS):
+                if self.matrizPiezas[fil][col] != 0:
+                    cont +=1
+            if cont == 10:
+                borrarFil.append(fil)
+            cont = 0
+                    
+        if borrarFil:
+            for borrar in borrarFil:
+                # Eliminar una línea
+                for cuadradito in self.matrizPiezas[borrar]:
+                    cuadradito.kill()
+                # Mover abajo los bloques
+                for fil in self.matrizPiezas:
+                    for cuadradito in fil: # Bajamos los cuadrados que esten por encima de la linea eliminada
+                        if cuadradito != 0 and cuadradito.pos.y < borrar:
+                            cuadradito.pos.y += 1
+                            
+                # Reconstruir la matriz
+                self.matrizPiezas = []
+                for fil in range(FILAS):
+                    self.matrizPiezas.append([])
+                    for col in range(COLUMNAS):
+                        self.matrizPiezas[fil].append(0)
+                        for cuadradito in self.grupo:
+                            if fil == cuadradito.pos.y and col == cuadradito.pos.x:
+                                self.matrizPiezas[fil][col] = cuadradito
+
+#                 for col in range(COLUMNAS):
+#                     self.matrizPiezas[borrar][col] = 0
+
     def run(self):
         # Actualizar
         self.grupo.update()
@@ -147,44 +199,53 @@ class Bloque(pygame.sprite.Sprite):
     
     def update(self):
         self.rect.topleft = self.pos * tamañoCeldas
+        
+    # Colisiones
+    # por limite del mapa o colisión con otra pieza
+    def colisionLateralIzq(self, X, matrizPieza):
+        if not(0 <= X) or (int(self.pos.y) >= 0 and matrizPieza[int(self.pos.y)][X]):
+            return True
+
+    def colisionLateralDer(self, X, matrizPieza):
+        if not(X < COLUMNAS) or (int(self.pos.y) >= 0 and matrizPieza[int(self.pos.y)][X]):
+            return True
+    def colisionInferior(self, Y, matrizPieza): 
+        if not(Y < FILAS) or (Y >= 0 and matrizPieza[Y][int(self.pos.x)]): 
+            return True
     
-    def colisionLateralIzq(self, X):
-        if not 0 <= X:
-            return True
-    def colisionLateralDer(self, X):
-        if not X < COLUMNAS:
-            return True
-    def colisionInferior(self, Y):
-        if not Y < FILAS:
-            return True
-        
-        
+    # Colisiones
+    def colisiones(self, cuadradoCentral):
+        distancia = self.pos - cuadradoCentral_pos
+        rotacion = distancia.rotate(90)
+        nuevaPos = cuadradoCentral + rotacion
 
 # Crea la clase Pieza, con las características de una pieza
 class Pieza():
-    def __init__(self, tipo, grupo):
+    def __init__(self, tipo, grupo, crearNuevaPieza, matrizPiezas):
         self.forma = piezas[tipo][0]
         self.color = piezas[tipo][1]
+        self.crearNuevaPieza = crearNuevaPieza # va a llamar a la función de la clase GAME
+        self.matrizPiezas = matrizPiezas
         # for de la pieza
         self.conjunto_pieza = [Bloque(grupo, pos, self.color) for pos in self.forma]
     
     # Colisiones
     def colisionLateralIzq(self, pieza):
-        colisionesIzq = [Bloque.colisionLateralIzq(cuadradito, int(cuadradito.pos.x - 1)) for cuadradito in self.conjunto_pieza]
+        colisionesIzq = [Bloque.colisionLateralIzq(cuadradito, int(cuadradito.pos.x - 1), self.matrizPiezas) for cuadradito in self.conjunto_pieza]
         if any(colisionesIzq) == True:
             return True
         else:
             return False
     
     def colisionLateralDer(self, pieza):
-        colisionesDer = [Bloque.colisionLateralDer(cuadradito, int(cuadradito.pos.x + 1)) for cuadradito in self.conjunto_pieza]
+        colisionesDer = [Bloque.colisionLateralDer(cuadradito, int(cuadradito.pos.x + 1), self.matrizPiezas) for cuadradito in self.conjunto_pieza]
         if any(colisionesDer) == True:
             return True
         else:
             return False
     
     def colisionInferior(self, pieza):
-        colisionesInf = [Bloque.colisionInferior(cuadradito, int(cuadradito.pos.y + 1)) for cuadradito in self.conjunto_pieza]
+        colisionesInf = [Bloque.colisionInferior(cuadradito, int(cuadradito.pos.y + 1), self.matrizPiezas) for cuadradito in self.conjunto_pieza]
         if any(colisionesInf) == True:
             return True
         else:
@@ -192,9 +253,13 @@ class Pieza():
 
     # Movimiento
     def moverbajo(self):
-        if self.colisionInferior(self.conjunto_pieza) != True:
+        if self.colisionInferior(self.conjunto_pieza) != True: # Si la función colisión retornase True es que hay algo debajo, y por eso en el Else para de moverse
             for cuadradito in self.conjunto_pieza:
                 cuadradito.pos.y += 1
+        else: # Llega al suela y se ejecuta la función para crear la nueva pieza
+            for cuadradito in self.conjunto_pieza:  # En la pos de cada cuadrado de una pieza dentro de la matriz guardo ese mismo cuadrado
+                self.matrizPiezas[int(cuadradito.pos.y)][int(cuadradito.pos.x)] = cuadradito
+            self.crearNuevaPieza()
 
     def moverizq(self):
         if self.colisionLateralIzq(self.conjunto_pieza) != True:
@@ -204,18 +269,17 @@ class Pieza():
         if self.colisionLateralDer(self.conjunto_pieza) != True:
             for cuadradito in self.conjunto_pieza:
                 cuadradito.pos.x += 1
-
+    # Rotación
+    def rotacion(self):
+        if self.tipo != 5: # Si la pieza no es un cuadrado
+            cuadradoCentral = self.conjunto_pieza[0].pos
+            nuevosBloques = []
+            for cuadradito in self.pieza:
+                Bloque.rotacion(cuadradoCentral)
     
-
-
-
-# Clase del Progama de pygame
+# Comienzo de Ejecución
 juego = True
-
-
-    
-        
-#general
+#general 
 pygame.init()
 display_surface = pygame.display.set_mode((LongVentana,AltVentana)) # Tamaño de la pantalla
 clock = pygame.time.Clock()
